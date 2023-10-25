5cea456f26dc99f7dc442752596f4b77e65596c8
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2023-10-25 14:41:21 +0200

    added END in ISA.txt line 76

--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -3,20 +3,27 @@ import chisel3.util._
 
 class RegisterFile extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
-    val aSel = Input(UInt(4.W))
-    val bSel = Input(UInt(4.W))
+    //Module Inputs
+    val aSel = Input(UInt(5.W))
+    val bSel = Input(UInt(5.W))
     val writeData = Input(UInt(32.W))
-    val writeSel = Input(UInt(4.W))
+    val writeSel = Input(UInt(5.W))
     val writeEnable = Input(Bool())
 
-    val a = Output(UInt(1.W))
-    val b = Output(UInt(1.W))
+    //Module Outputs
+    val a = Output(UInt(32.W))
+    val b = Output(UInt(32.W))
+
+    //Array of 15 Register
+    val registerFile = Reg(Vec(15, UInt(15.W)))
   })
 
   //Implement this module here
-  
+  when (writeEnable) {
+    
+    
+
+  }
   
 
-  
 }
