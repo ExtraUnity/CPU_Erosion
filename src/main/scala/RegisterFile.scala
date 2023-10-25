cf14cf25f5b012274cf67fc3054bded7c74e9754
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2023-10-25 13:58:03 +0200

    Defined modules

--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -4,8 +4,19 @@ import chisel3.util._
 class RegisterFile extends Module {
   val io = IO(new Bundle {
     //Define the module interface here (inputs/outputs)
+    val aSel = Input(UInt(4.W))
+    val bSel = Input(UInt(4.W))
+    val writeData = Input(UInt(32.W))
+    val writeSel = Input(UInt(4.W))
+    val writeEnable = Input(Bool())
+
+    val a = Output(UInt(1.W))
+    val b = Output(UInt(1.W))
   })
 
   //Implement this module here
+  
+  
 
+  
 }
