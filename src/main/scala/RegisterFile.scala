dabc182644f84e29940bc7ec41f2ee59ce2806b1
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2023-10-25 14:52:23 +0200

    Task 7: RegisterFile done (need test)

--- src/main/scala/RegisterFile.scala
+++ src/main/scala/RegisterFile.scala
@@ -4,10 +4,10 @@ import chisel3.util._
 class RegisterFile extends Module {
   val io = IO(new Bundle {
     //Module Inputs
-    val aSel = Input(UInt(5.W))
-    val bSel = Input(UInt(5.W))
+    val aSel = Input(UInt(4.W))
+    val bSel = Input(UInt(4.W))
     val writeData = Input(UInt(32.W))
-    val writeSel = Input(UInt(5.W))
+    val writeSel = Input(UInt(4.W))
     val writeEnable = Input(Bool())
 
     //Module Outputs
@@ -15,14 +15,14 @@ class RegisterFile extends Module {
     val b = Output(UInt(32.W))
 
     //Array of 15 Register
-    val registerFile = Reg(Vec(15, UInt(15.W)))
+    val registerFile = Reg(Vec(16, UInt(32.W)))
   })
 
   //Implement this module here
+  io.a := registerFile(io.aSel)
+  io.b := registerFile(io.bSel)
   when (writeEnable) {
-    
-    
-
+    registerFile(writeSel):= writeData
   }
   
 
